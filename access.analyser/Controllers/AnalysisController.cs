2892fdf495c175f0b75ef2db159d0e09d1b4380d
Piotr Kowalski <Piotr.Kowalski.1998@protonmail.com>
2020-05-17 22:15:25 +0200

    Add view to setup analysis

---
+++ access.analyser/Controllers/AnalysisController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace access.analyser.Controllers
+{
+    [Authorize]
+    public class AnalysisController : Controller
+    {
+        /// <summary>
+        /// Allows the setup of analysis
+        /// </summary>
+        /// <returns>Index view</returns>
+        public IActionResult Index () => View ();
+    }
+}
