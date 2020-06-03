d4742a5778bf1e27158d4d6a84d3eff481580119
k1kli <karolklimaszewski0@gmail.com>
2020-06-03 11:29:36 +0200

    Update scripts

--- scripts/start_app.sh
+++ scripts/start_app.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 cd /home/ec2-user/accessanalyserapp
 rm access.analyser.deps.json #if not deleted app requires Microsoft.Data.SqlClient.dll which is present
-nohup dotnet access.analyser.dll --urls="http://0.0.0.0:5000;https://0.0.0.0:5001" &>/dev/null &
+rm /home/ec2-user/startlog
+nohup dotnet access.analyser.dll --urls="http://0.0.0.0:5000;https://0.0.0.0:5001" >/home/ec2-user/startlog 2>&1 &
